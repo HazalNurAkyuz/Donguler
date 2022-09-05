ecc4af24fcf7c1162072a5d3c01cd43728a9f10f
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-09-05 10:31:31 +0300

    Initial commit

---
+++ src/WhileDongusu.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Döngü Başladı");
+        while (i <= 5) {
+            System.out.print(i + ",");
+            i++;
+        }
+        System.out.println("\nDöngü Bitti");
+    }
+}
